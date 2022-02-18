6bdc06331f2f7ef62b076470aa9d8cb1001e8e61
Arisch <arischvaran00@gmail.com>
2022-02-18 17:08:06 +0800

    Added hex color codes as well

--- Background Color Changer/script.js
+++ Background Color Changer/script.js
@@ -1,7 +1,36 @@
 const btnChange = document.getElementById("btn-change");
-const colorCodeOutput = document.getElementById("colorCodeOutput");
+const colorRGB = document.querySelector(".colorRGB");
+const colorHex = document.querySelector(".colorHex");
 
 btnChange.addEventListener("click", () => {
-    const colorCode = document.body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
-    colorCodeOutput.innerHTML = `Color: ${colorCode}`;
+    // color code in rgb
+    let red = Math.floor(Math.random() * 255) + 1;
+    let green = Math.floor(Math.random() * 255) + 1;
+    let blue = Math.floor(Math.random() * 255) + 1;
+
+    const colorCode = `rgb(${red}, ${green}, ${blue})`;
+    colorRGB.textContent = `RGB: ${colorCode}`;
+
+    // color code in hex
+    colorHex.textContent = `Hex: ${concatHex(red, green, blue)}`;
+
+    // set background color
+    document.body.style.backgroundColor = colorCode;
 });
+
+/* 
+next is to change the color code from rgb to hex
+- rgb has 256 values and hex has 6 letters (2 account for each color)
+- so in hex (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, A, B, C, D, E, F) = 16 * 16 = 256 in rgb
+- take the value from rgb and divide 16
+- if value more than 10 then use switch case to convert to hex value
+*/
+
+function toHex(value){
+    let hexadecimal = value.toString(16);
+    return hexadecimal.length == 1 ? `0${hexadecimal.toUpperCase()}`: hexadecimal.toUpperCase();
+}
+
+function concatHex(red, green, blue){
+    return '#' + toHex(red) + toHex(green) + toHex(blue);
+}
